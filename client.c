0b2e72cad11416f98847bc6ccd41dbaa7f38e0de
Ivan Aksenenko <ioaksenenko@gmail.com>
2021-03-06 11:47:03 +0000

    first commit

---
+++ client.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "erproc.h"
+
+int main() {
+    int fd = Socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(34543);
+    Inet_pton(AF_INET, "127.0.0.1", (struct sockaddr *) &addr.sin_addr);
+    Connect(fd, (struct sockaddr *) &addr, sizeof addr);
+    char filename[256];
+    printf("Enter the name of the file you want to delete: ");
+    scanf("%s", filename);
+    write(fd, filename, strlen(filename));
+    char buf[256];
+    int nread;
+    nread = read(fd, buf, 256);
+    if (nread == -1) {
+    	perror("read failed");
+	exit(EXIT_FAILURE);
+    }
+    if (nread == 0) {
+    	printf("EOF occured\n");
+    }
+    write(STDOUT_FILENO, buf, nread);
+    close(fd);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include "erproc.h"
+
+int main() {
+    int server = Socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(34543);
+    Bind(server, (struct sockaddr *) &addr, sizeof addr);
+    Listen(server, 5);
+    socklen_t addrlen = sizeof addr;
+    int client = Accept(server, (struct sockaddr *) &addr, &addrlen);
+    int nread;
+    char filename[256];
+    nread = read(client, filename, 256);
+    if (nread == -1) {
+    	perror("reed failed");
+	exit(EXIT_FAILURE);
+    }
+    if (nread == 0) {
+    	printf("END OF FILE occured\n");
+    }
+    Remove(filename);
+    char response[512];
+    sprintf(response, "File '%s' deleted successfully.\n", filename);
+    write(client, response, strlen(response));
+    sleep(1);
+    close(client);
+    close(server);
+    return 0;
+}
